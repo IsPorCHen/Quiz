c89ffac87538c5bcd55d7db6146bb77c90278d03
IsPorCHen <105991525+IsPorCHen@users.noreply.github.com>
2024-12-19 00:43:52 +0500

    Update save_results.php

--- save_results.php
+++ save_results.php
@@ -2,19 +2,16 @@
 session_start();
 include 'includes/db_connect.php';
 
-// Проверка, что пользователь авторизован
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit;
 }
 
-// Получение результатов викторины (например, из формы или через POST-запрос)
 $quiz_id = $_POST['quiz_id'];
-$score = $_POST['score']; // Здесь $score может быть вычислен на основе правильных ответов
+$score = $_POST['score'];
 
 $user_id = $_SESSION['user_id'];
 
-// Сохранение результата в базе данных
 $query = "INSERT INTO results (user_id, quiz_id, score) VALUES ('$user_id', '$quiz_id', '$score')";
 if (mysqli_query($conn, $query)) {
     echo "Результаты сохранены!";
