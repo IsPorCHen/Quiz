5abac9b9a0918a453c71b293466b87101ded567c
IsPorCHen <105991525+IsPorCHen@users.noreply.github.com>
2024-12-19 00:43:30 +0500

    Update register.php

--- register.php
+++ register.php
@@ -5,20 +5,18 @@ include 'includes/db_connect.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Хэширование пароля
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    // Проверка, существует ли уже пользователь с таким email
     $query = "SELECT * FROM users WHERE email = '$email'";
     $result = mysqli_query($conn, $query);
     
     if (mysqli_num_rows($result) > 0) {
         echo "Пользователь с таким email уже существует!";
     } else {
-        // Добавление нового пользователя
         $query = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
         if (mysqli_query($conn, $query)) {
             echo "Регистрация прошла успешно!";
-            header("Location: login.php"); // Перенаправление на страницу входа
+            header("Location: login.php");
         } else {
             echo "Ошибка регистрации!";
         }
